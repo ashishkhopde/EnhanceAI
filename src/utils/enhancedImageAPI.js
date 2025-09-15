8084cc72d2b1765beadb3ec1b255251215d2c518
ashishkhopde <ashishkhopde7089@gmail.com>
2025-09-15 17:00:18 +0530

    update

--- src/utils/enhancedImageAPI.js
+++ src/utils/enhancedImageAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = 'https://techhk.aoscdn.com/';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const enhancedImageAPI = async (file) => {
